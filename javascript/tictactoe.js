3d2cdaf60f8d0b72f915b09a1f5b297b7bdca9f1
Adam Shuvalov <adam.shuvalov@gmail.com>
2023-04-05 11:52:40 +1000

    added animations and changed X and O iamge

--- javascript/tictactoe.js
+++ javascript/tictactoe.js
@@ -80,16 +80,16 @@ $('document').ready(function (){
     $('#game-box').on('click', function(event) {
         let clickedBox = event.target.id;
 
-        if (turnCount < 9 && !result && !board[clickedBox]) {
+        if (turnCount < 9 && !result && !board[clickedBox] && $(event.target).hasClass('grid-box')) {
             if (turn === 'X') {
                 board[clickedBox]= 'X'
-                $(`#${clickedBox}`).addClass('x')
+                $(`#${clickedBox}`).addClass('x animate__fadeIn')
                 turn = 'O'
                 turnCount++
                 determineWinner()
             } else if (turn === 'O') {
                 board[clickedBox]= 'O'
-                $(`#${clickedBox}`).addClass('o')
+                $(`#${clickedBox}`).addClass('o animate__fadeIn')
                 turn = 'X'
                 turnCount++
                 determineWinner()
@@ -113,65 +113,30 @@ $('document').ready(function (){
     //Button to reset board --- resetButton is added dynamically, so jQuery must retrieve the 'header in the first instance of loading the page.
     $('header').on('click', function (event){
         if (event.target.id === 'resetButton'){
+
             for (let i = 1; i <= 9; i++) {
                 board[`box${i}`] = ''
             }
             console.log(board)
-            $('.grid-box').removeClass('o x');
+            $('.grid-box').removeClass('o x animate__fadeIn');
             turn = 'X';
             result = '';
             turnCount = 0;
+            
             $('#resetButton').addClass('animate__fadeOut');
             setTimeout(function () {
                 resetButton.remove()}, 1500
-            )
+            );
             $('#printedResult').addClass('animate__fadeOut');
             setTimeout(function () {
                 $('#printedResult').remove()}, 1500
-            )
+            );
         }
     })
 })
 
-// HTML&CSS
-    // Board is created with divs 
-    // Changing the class of the div makes them display an X or O
-    // Use on.('click', ...) on the div to change their class
-      // define a variable for both players 'player1' and 'player2'
-        // define a variable name 'turn' thats value is either player1 or player2.
-        // create an object that holds a key value pair for every position on the board
-
-
-    // const player1 = x;
-    // const player2 = o;
-
-// LOGIC
-
-  //Pos value is either X or O
-    // These values can be changed on.(click), their value determines the class of a div, that will then determine whether the div shows an X or 0
-
-    //When variable 'turn'==='player1' the next on.(click) changes the divs to an X, the same anonymous function will change the value of 'turn' to 'player2'
-
-    //When variable 'turn'==='player2' the next on.(click) changes the divs to an O, the same anonymous function will change the value of 'turn' to 'player1'
-
-    //How do we determine a result?
-        // After every placement, a function will check through all possible permutations of 3 key value pairs in a line. If the function finds 3 equal values a result will be determined. (This can possibly be done with a for loop)
-
-    //How do we decide who goes first?
-        //  A button that on.('click', ...), changes the turn variable that decides whose turn it is.
-
-//code im saving -->
-
- // if (turn === 'X' && turnCount < 9 && !result && !board[clickedBox]) {
-        //     board[clickedBox]= 'X'
-        //     $(`#${clickedBox}`).addClass('x')
-        //     turn = 'O'
-        //     turnCount++
-        //     determineWinner()
-        // } else if (turn === 'O' && turnCount < 9 && !result && !board[clickedBox]) {
-        //     board[clickedBox]= 'O'
-        //     $(`#${clickedBox}`).addClass('o')
-        //     turn = 'X'
-        //     turnCount++
-        //     determineWinner()
-        // }
+// Things to check and do:
+    //  1. You can click on the backbaord and change the background image of that, this can be replicated when I click drag and release my mouse
+    //  2. Animate in and out the X and Os
+    //  3. Relate the X and Os to the UI (score etc)
+    //  4. Create option to play AI
